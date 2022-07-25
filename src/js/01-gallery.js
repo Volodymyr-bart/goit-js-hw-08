09449facb69c7361fa5631ba8f1f2ce7d21e953d
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-07-25 22:05:21 +0300

    add js with 07

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,26 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListDiv = document.querySelector('.gallery');
+const cardsMarkup = createPictureCards(galleryItems);
+
+galleryListDiv.insertAdjacentHTML('afterbegin', cardsMarkup);
+galleryListDiv.addEventListener('click', onPaletteContainerClick);
+
+function createPictureCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>`;
+    })
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  // captions: true,
+  captionDelay: 250,
+  // captionsData: title,
+  overlayOpacity: 1,
+});
