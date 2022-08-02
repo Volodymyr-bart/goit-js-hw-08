2e5da977a31f0004ec56d8513af74a7d7ca97cac
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-08-02 19:06:23 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,38 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('.feedback-form textarea'),
 };
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', onTextareaInput);
+const formData = {};
 
-console.log(refs.form);
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value;
+  console.log(formData);
+});
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
 
 function onFormSubmit(e) {
   e.preventDefault();
+  console.log('poletelo');
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
 function onTextareaInput(e) {
-  const message = e.currentTarget.value;
-  localStorage.setItem('feedback-form-state', message);
+  let message = e.target.value;
+  localStorage.setItem(STORAGE_KEY, message);
 }
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    console.log(savedMessage);
+    refs.textarea.value = savedMessage;
+  }
+}
+populateTextarea();
