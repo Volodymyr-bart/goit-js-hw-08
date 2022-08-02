711d26f6da6be91bddae4ce6d3d368d5b568a223
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-08-02 12:19:18 +0300

    start task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+console.log(refs.form);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+}
+function onTextareaInput(e) {
+  const message = e.currentTarget.value;
+  localStorage.setItem('feedback-form-state', message);
+}
